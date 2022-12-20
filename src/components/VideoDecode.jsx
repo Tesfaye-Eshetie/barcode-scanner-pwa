8d41ceda7bb693d99850f25977a0e0dbecdc28be
Tesfaye Eshetie <73901218+Tesfaye-Eshetie@users.noreply.github.com>
2022-12-20 16:37:41 -0500

    fixed bugs

--- src/components/VideoDecode.jsx
+++ src/components/VideoDecode.jsx
@@ -18,9 +18,6 @@ export default function VideoDecode({ setBarcode }) {
             console.log(result.barcodeText);
           }
         };
-        scanner.onUniqueRead = (txt) => {
-          alert(txt);
-        };
         await scanner.open();
       } catch (ex) {
         let errMsg;
@@ -30,21 +27,19 @@ export default function VideoDecode({ setBarcode }) {
         } else {
           errMsg = ex.message || ex;
         }
-        console.error(errMsg);
         alert(errMsg);
       }
     })();
   }, []);
 
   return (
-    <div ref={elRef} className="videoContainer">
+    <div ref={elRef} className="video-container">
       <div className="div-ui-container">
         <svg className="dce-bg-loading" viewBox="0 0 1792 1792">
           <path d="M1760 896q0 176-68.5 336t-184 275.5-275.5 184-336 68.5-336-68.5-275.5-184-184-275.5-68.5-336q0-213 97-398.5t265-305.5 374-151v228q-221 45-366.5 221t-145.5 406q0 130 51 248.5t136.5 204 204 136.5 248.5 51 248.5-51 204-136.5 136.5-204 51-248.5q0-230-145.5-406t-366.5-221v-228q206 31 374 151t265 305.5 97 398.5z"></path>
         </svg>
-        <div className="dce-video-container">
-          <div className="dce-video-container-center"></div>
-        </div>
+        <div className="dce-video-container"></div>
+        <div className="div-ui-container-center"></div>
       </div>
     </div>
   );
