10e76162084041c58b24536b7fe6d42d1f5fe281
Tesfaye Eshetie <73901218+Tesfaye-Eshetie@users.noreply.github.com>
2022-12-21 18:24:56 -0500

    removed pwa

--- src/components/VideoDecode.jsx
+++ src/components/VideoDecode.jsx
@@ -3,8 +3,7 @@ import { BarcodeScanner } from "dynamsoft-javascript-barcode";
 
 import "./VideoDecode.css";
 
-export default function VideoDecode({ setBarcode, showScanner }) {
-  const [fullScreen, setFullScreen] = useState("");
+export default function VideoDecode({ setBarcode }) {
   const elRef = useRef();
 
   useEffect(() => {
@@ -30,18 +29,11 @@ export default function VideoDecode({ setBarcode, showScanner }) {
         alert(errMsg);
       }
     })();
-  }, [showScanner]);
-
-  useEffect(() => {
-    window.addEventListener("resize", () => setFullScreen("full-screen"));
-    return () => {
-      window.removeEventListener("resize", () => setFullScreen("full-screen"));
-    };
-  }, [showScanner]);
+  }, []);
 
   return (
     <div ref={elRef} className="video-container">
-      <div className={`div-ui-container ${fullScreen}`}>
+      <div className="div-ui-container">
         <svg className="dce-bg-loading" viewBox="0 0 1792 1792">
           <path d="M1760 896q0 176-68.5 336t-184 275.5-275.5 184-336 68.5-336-68.5-275.5-184-184-275.5-68.5-336q0-213 97-398.5t265-305.5 374-151v228q-221 45-366.5 221t-145.5 406q0 130 51 248.5t136.5 204 204 136.5 248.5 51 248.5-51 204-136.5 136.5-204 51-248.5q0-230-145.5-406t-366.5-221v-228q206 31 374 151t265 305.5 97 398.5z"></path>
         </svg>
