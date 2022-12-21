16a343c2943b62df4191a80c6de3a4c1fc503b3c
Tesfaye Eshetie <73901218+Tesfaye-Eshetie@users.noreply.github.com>
2022-12-20 21:38:05 -0500

    updated layout

--- src/components/VideoDecode.jsx
+++ src/components/VideoDecode.jsx
@@ -11,6 +11,8 @@ export default function VideoDecode({ setBarcode }) {
     (async () => {
       try {
         const scanner = await BarcodeScanner.createInstance();
+        const resizeEvent = new Event("resize");
+        window.dispatchEvent(resizeEvent);
         await scanner.setUIElement(elRef.current);
         scanner.onFrameRead = (results) => {
           for (let result of results) {
