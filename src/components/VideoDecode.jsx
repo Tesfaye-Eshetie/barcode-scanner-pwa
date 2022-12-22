ec96f79fae65bb348c615e8834a760d54646cff2
Tesfaye Eshetie <73901218+Tesfaye-Eshetie@users.noreply.github.com>
2022-12-22 11:56:23 -0500

    used useLayoutEffect hooks

--- src/components/VideoDecode.jsx
+++ src/components/VideoDecode.jsx
@@ -1,32 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { BarcodeScanner } from "dynamsoft-javascript-barcode";
 
 import "./VideoDecode.css";
 
 export default function VideoDecode({ setBarcode }) {
-  const [windowDimenion, detectHW] = useState({
-    winWidth: window.innerWidth,
-    winHeight: window.innerHeight,
-  });
-
   const elRef = useRef();
 
-  const detectSize = () => {
-    detectHW({
-      winWidth: window.innerWidth,
-      winHeight: window.innerHeight,
-    });
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", detectSize);
-
-    return () => {
-      window.removeEventListener("resize", detectSize);
-    };
-  }, [windowDimenion]);
-
-  useEffect(() => {
+  useLayoutEffect(() => {
     (async () => {
       try {
         const scanner = await BarcodeScanner.createInstance();
@@ -53,13 +33,7 @@ export default function VideoDecode({ setBarcode }) {
 
   return (
     <div ref={elRef} className="video-container">
-      <div
-        className="div-ui-container"
-        style={{
-          width: windowDimenion.winWidth,
-          height: windowDimenion.winHeight,
-        }}
-      >
+      <div className="div-ui-container">
         <svg className="dce-bg-loading" viewBox="0 0 1792 1792">
           <path d="M1760 896q0 176-68.5 336t-184 275.5-275.5 184-336 68.5-336-68.5-275.5-184-184-275.5-68.5-336q0-213 97-398.5t265-305.5 374-151v228q-221 45-366.5 221t-145.5 406q0 130 51 248.5t136.5 204 204 136.5 248.5 51 248.5-51 204-136.5 136.5-204 51-248.5q0-230-145.5-406t-366.5-221v-228q206 31 374 151t265 305.5 97 398.5z"></path>
         </svg>
